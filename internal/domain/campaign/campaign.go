082dfd19f5b789db4a951d84734b00be8ffe4730
Filipe Mattos <filipemagioni@outlook.com>
2025-02-11 22:17:53 -0300

    add Campaign domain and commit initial go.mod file

---
+++ internal/domain/campaign/campaign.go
@@ -0,0 +1,16 @@
+// Dominio para campanha de email
+package campaign
+
+import "time"
+
+type Contact struct {
+	Email string
+}
+
+type Campaign struct {
+	ID        string
+	Name      string
+	CreatedOn time.Time
+	Content   string
+	Contacts  []Contact
+}
